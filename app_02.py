4eaa9fc42559758d98727457f74728aa2a404df1
KVAlwaysLearning <codingacc979@gmail.com>
2026-06-19 20:06:18 +0530

    Update app_02.py

--- app_02.py
+++ app_02.py
@@ -435,7 +435,6 @@ if app_mode == "🏠​ Home":
 
     blur_text(
         "Interactive Dashboard Covering All Products!!",
-        "",
         "Machine Learning Model based Customer Segmentation",
         tag="p", extra_class=""
     )

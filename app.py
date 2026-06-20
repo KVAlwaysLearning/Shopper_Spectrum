e6ab1ad77929965e894c65aebd10ed5357e9c6c8
KVAlwaysLearning <codingacc979@gmail.com>
2026-06-20 22:39:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -485,15 +485,6 @@ if app_mode == "Home":
         
     ], height=40, font_size=18)
 
-    blur_text(
-        f"Interactive Dashboard Covering All Products!!",
-        tag="p", extra_class=""
-    )
-
-    blur_text(
-        "Machine Learning Model based Customer Segmentation",
-        tag="p", extra_class=""
-    )
 
     st.markdown("<br>", unsafe_allow_html=True)
     eyebrow("SEGMENT CONSTELLATION")
